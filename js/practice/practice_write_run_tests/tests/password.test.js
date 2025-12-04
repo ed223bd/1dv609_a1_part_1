6aaba4386480ebd2257b083e992c799e512a7e92
Ebba Dahlberg <ed223bd@student.lnu.se>
2025-12-04 10:28:19 +0100

    New format for test file

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -11,102 +11,103 @@
 // import { Password } from '../src/BugVeryShort' //✅
 // import { Password } from '../src/BugWrongHashingAlgorithm' //✅
 // import { Password } from '../src/BugWrongMessage' //✅
-// import { Password } from '../src/Correct'
+import { Password } from '../src/Correct'
 
-// describe('BugToShortPassword, 11 characters', () => {
-//     const password = 'Hej12345678';
+describe('BugToShortPassword, 11 characters', () => {
+    const password11 = 'Hej12345678';
 
-//     test('should throw error when less than 12 characters', () => {
-//         // En funktion, men ANONYM. En ANONYM funktion
-//         // skulle returnera resultatet av funktionen, inte
-//         // funktionen i sig.
-//         expect(() => new Password(password)).toThrow(Error);
-//     });
-// })
+    test('should throw error when less than 12 characters', () => {
+        // En funktion, men ANONYM. En ANONYM funktion
+        // skulle returnera resultatet av funktionen, inte 
+        // funktionen i sig.
+        expect(() => new Password(password11)).toThrow(Error);
+    });
 
-// describe('BugVeryShort, 6 characters', () => {
-//     const password = 'Hej123'
+})
 
-//     test('should throw error when less than 6 characters', () => {
-//         expect(() => new Password(password)).toThrow(Error)
-//     })
-// })
+describe('BugVeryShort, 6 characters', () => {
+    const password = 'Hej123'
 
-// describe('BugNeverContainsNumbers', () => {
-//     const password = 'hejsansvejsan12'
+    test('should throw error when less than 6 characters', () => {
+        expect(() => new Password(password)).toThrow(Error)
+    })
+})
 
-//     test('should not throw error if password has numbers', () => {
-//         expect(() => new Password(password)).not.toThrow(Error)
-//     })
-// })
+describe('BugNeverContainsNumbers', () => {
+    const password = 'hejsansvejsan12'
 
-// describe('BugMissingPasswordCheck', () => {
-//     const password = 'hej1'
+    test('should not throw error if password has numbers', () => {
+        expect(() => new Password(password)).not.toThrow(Error)
+    })
+})
 
-//     test('should throw error when password is less than 12 characters', () => {
-//         expect(() => new Password(password)).toThrow(Error)
-//     })
-// })
+describe('BugMissingPasswordCheck', () => {
+    const password = 'hej1'
 
-// describe('BugMissingNumberCheck', () => {
-//     const password = 'hejsansvejsan'
+    test('should throw error when password is less than 12 characters', () => {
+        expect(() => new Password(password)).toThrow(Error)
+    })
+})
 
-//     test('should throw error if there are no numbers', () => {
-//         expect(() => new Password(password)).toThrow(Error)
-//     })
-// })
+describe('BugMissingNumberCheck', () => {
+    const password = 'hejsansvejsan'
 
-// describe('BugDoesNotTrim', () => {
-//     const password = ' Hej1234567 '
+    test('should throw error if there are no numbers', () => {
+        expect(() => new Password(password)).toThrow(Error)
+    })
+})
 
-//     test('should throw error when password with whitespaces', () => {
-//         expect(() => new Password(password)).toThrow(Error)
-//     })
-// })
+describe('BugDoesNotTrim', () => {
+    const password = ' Hej1234567 '
 
-// describe ('BugDoesNotHash', () => {
-//     const password = 'hej123456789'
-//     const passwordObject = new Password(password)
-//     const hash = passwordObject.getPasswordHash()
+    test('should throw error when password with whitespaces', () => {
+        expect(() => new Password(password)).toThrow(Error)
+    })
+})
 
-//     test('should have a hash value of type number', () => {
-//         expect(hash).not.toBe(password)
-//     })
-// })
+describe('BugDoesNotHash', () => {
+    const password = 'hej123456789'
+    const passwordObject = new Password(password)
+    const hash = passwordObject.getPasswordHash()
 
-// describe('BugWrongHashingAlgorithm', () => {
-//     const password1 = 'Hej123456789'
-//     const password2 = 'Hej987654321'
+    test('should have a hash value of type number', () => {
+        expect(hash).not.toBe(password)
+    })
+})
 
-//     const passwordObject1 = new Password(password1)
-//     const passwordObject2 = new Password(password2)
+describe('BugWrongHashingAlgorithm', () => {
+    const password1 = 'Hej123456789'
+    const password2 = 'Hej987654321'
 
-//     const hash1 = passwordObject1.getPasswordHash()
-//     const hash2 = passwordObject2.getPasswordHash()
+    const passwordObject1 = new Password(password1)
+    const passwordObject2 = new Password(password2)
 
-//     test('should create different hashes for different passwords', () => {
-//         expect(hash1).not.toBe(hash2)
-//     })
-// })
+    const hash1 = passwordObject1.getPasswordHash()
+    const hash2 = passwordObject2.getPasswordHash()
 
-// describe('BugisPasswordAlwaysSame', () => {
-//     const password1 = 'Hej123456789'
-//     const password2 = 'Hej987654321'
+    test('should create different hashes for different passwords', () => {
+        expect(hash1).not.toBe(hash2)
+    })
+})
 
-//     const passwordObject1 = new Password(password1)
-//     const passwordObject2 = new Password(password2)
+describe('BugisPasswordAlwaysSame', () => {
+    const password1 = 'Hej123456789'
+    const password2 = 'Hej987654321'
 
-//     const same = passwordObject1.isPasswordSame(passwordObject2)
+    const passwordObject1 = new Password(password1)
+    const passwordObject2 = new Password(password2)
 
-//     test('should return false if passwords are different', () => {
-//         expect(same).toBeFalsy()
-//     })
-// })
+    const same = passwordObject1.isPasswordSame(passwordObject2)
 
-// describe('BugWrongMessage', () => {
-//     const password = 'Hej123'
+    test('should return false if passwords are different', () => {
+        expect(same).toBeFalsy()
+    })
+})
 
-//     test('should throw "Too short password" if password is less than 12 characters', () => {
-//         expect(() => new Password(password)).toThrow('Too short password')
-//     })
-// })
+describe('BugWrongMessage', () => {
+    const password = 'Hej123'
+
+    test('should throw "Too short password" if password is less than 12 characters', () => {
+        expect(() => new Password(password)).toThrow('Too short password')
+    })
+})
